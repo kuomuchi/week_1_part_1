b2978e65a12e89db07575c67bec9d6682cc22d64
kuonuchi <kuokillua1228@gmail.com>
2021-03-26 15:22:52 +0800

    add home page

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const { send } = require('process');
+const path = require('path');
 const app = express();
 
+app.use(express.static('public'));
+
+
+// app.set('public engine', 'html');
+
 app.get('/', (req, res) =>{
-  res.send('YAAAAAA');
+  res.sendFile(__dirname + '/public/welcome.html');
+
 });
 
 app.listen(3000, () =>{
