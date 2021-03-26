065e50611a8a874d93728d76ffecdf66206e87e4
kuonuchi <kuokillua1228@gmail.com>
2021-03-26 13:33:05 +0800

    add gitignore

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const { send } = require('process');
 const app = express();
 
-app.listen(3000, ()=>{
-  console.log('80 ready');
-});
-
 app.get('/', (req, res) =>{
   res.send('YAAAAAA');
+});
+
+app.listen(3000, () =>{
+  console.log('run on 3000');
 });
