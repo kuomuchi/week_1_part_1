39d2f0e07c3518e0c4076dfadf80e4aff6c2bd43
kuonuchi <kuokillua1228@gmail.com>
2021-03-26 15:30:03 +0800

    fix home page

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const { send } = require('process');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+app.use('/admin', express.static('public'));
 
 
 // app.set('public engine', 'html');
 
 app.get('/', (req, res) =>{
-  res.sendFile(__dirname + '/public/welcome.html');
+  // res.sendFile(__dirname + '/public/welcome.html');
+  res.send('welcome');
 
 });
 
