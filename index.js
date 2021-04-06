02d8954ebb59880502ef52aeba54776cdf5c1398
kuonuchi <kuokillua1228@gmail.com>
2021-04-06 11:29:13 +0800

    fix console req.body

--- index.js
+++ index.js
@@ -387,7 +387,6 @@ app.get('/api/1.0/user/signup', (req, res)=>{
 
 //登入
 app.post('/api/1.0/user/signin', async(req, res)=>{
-  console.log('進入signin');
   console.log(req.body);
   const {provider, access_token} = req.body;
   
