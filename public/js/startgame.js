468285a6230aa7cad5149d8daaf0ae88c5d92984
kuonuchi <kuokillua1228@gmail.com>
2021-04-16 18:04:32 +0800

    fix

--- public/js/startgame.js
+++ public/js/startgame.js
@@ -1,4 +1,4 @@
-const socket = io('http://3.13.254.132', { transports: ['websocket'] })
+const socket = io.connect()
 const player = window.localStorage.getItem('player')
 let nowTopic = -1
 let ans
