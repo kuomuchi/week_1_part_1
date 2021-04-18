786ac2fe3bb5907d96a01b496f946d44e55ea753
kuonuchi <kuokillua1228@gmail.com>
2021-04-18 21:03:26 +0800

    fix

--- public/js/powerpoint.js
+++ public/js/powerpoint.js
@@ -4,6 +4,7 @@ document.getElementById('summit').addEventListener('click', () => {
   if (document.getElementById('input').value === '') {
     alert('請別填空')
   } else {
+    alert('已將連結送出')
     const inputValue = document.getElementById('input').value
     socket.emit('postPowerPoint', inputValue)
     document.getElementById('input').value = ''
