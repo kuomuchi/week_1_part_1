30d1a484fe70df8c7bcaa318e4704184be99a789
kuonuchi <kuokillua1228@gmail.com>
2021-04-24 17:46:52 +0800

    fix linebot

--- line.js
+++ line.js
@@ -14,3 +14,21 @@ const linebotParser = bot.parser()
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+setTimeout(function () {
+  bot.push('U813645541c262fb6d9c967efeb884aeb', '啟動了！')
+}, 0)
+
+app.listen(process.env.PORT || 80, function () {
+  console.log('LineBot is running.')
+})
+
+bot.on('follow', function (event) {
+  event.reply('你好！謝謝你加我好友 :D')
+  bot.push(me, '有人加我好友耶！\n我的朋友有一天會比你多的 >:D')
+})
+
+bot.on('join', function (event) {
+  event.reply('嗨')
+  bot.push('U813645541c262fb6d9c967efeb884aeb', '有人加我到群組裡耶 :)')
+})
